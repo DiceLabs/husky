9ff2c72a7d8cc790e25dbf3d463ae0ba891f2dd4
Dice Labs <dicelabs99@gmail.com>
2024-01-17 15:02:13 -0800

    Separate gripper API from key listener

--- src/component_apis/grippers/grippers.py
+++ src/component_apis/grippers/grippers.py
@@ -3,9 +3,7 @@
 """ 
     @Zix
     Gripper API
-    This node will listen for key presses which are matched with desired commands of the grippers.
-    Uses the ASDF keys for closing and opening of left and right grippers by publishing the correct message to the 
-    Gripper action server in real time.
+    Defines Easy ROS Interface to call common movements for grippers (open, close ...)
     Must be run from environment with correct ROS msgs set up, the husky will have this environment on its on-board computer
 """
 
@@ -18,22 +16,23 @@ from pynput.keyboard import Key
 LEFT_GRIPPER_TOPIC = '/left_gripper/command_robotiq_action/goal'
 RIGHT_GRIPPER_TOPIC = '/right_gripper/command_robotiq_action/goal'
 
+""" Define movement constants """
 DEFAULT_FORCE = 5.0	# Reasonable amount of force
 
-# speed calue limits from 0.01 to 0.1
 SPEED_MIN = 0.01
 SPEED_MAX = 0.1
 DEFAULT_SPEED = SPEED_MIN
 
-# position value limits from 0.00 to 0.085
 POS_MIN = 0.00
 POS_MAX = 0.085
 POS_RANGE = POS_MAX - POS_MIN
 POS_MID = POS_RANGE / 2
+""" Define movement constants """
 
 EMPTY_ID = ''
 DEFAULT_SEQ_NUM = 0
 
+""" namespace MessageFilling() """
 def create_msg(position):
     msg = CommandRobotiqGripperActionGoal()
     fill_header(msg)
@@ -59,57 +58,21 @@ def fill_goal(msg, position):
     msg.goal.position = position	
     msg.goal.speed = DEFAULT_SPEED	
     msg.goal.force = DEFAULT_FORCE
-
-def close_gripper(publisher):
-    msg = create_msg(POS_MIN)
-    publisher.publish(msg)
-
-def open_gripper(publisher):
-    msg = create_msg(POS_MAX)
-    publisher.publish(msg)
+""" namespace MessageFilling() """
 
 class GripperNode():
     def __init__(self):
         self.left_pub = rospy.Publisher(LEFT_GRIPPER_TOPIC, CommandRobotiqGripperActionGoal, queue_size=10)
         self.right_pub = rospy.Publisher(RIGHT_GRIPPER_TOPIC, CommandRobotiqGripperActionGoal, queue_size=10)
 
-def on_key_press(key, gripper_node):
-    try:
-        if key.char == 'a':
-            close_gripper(gripper_node.left_pub)
-        elif key.char == 's':
-            open_gripper(gripper_node.left_pub)
-        elif key.char == 'd':
-            close_gripper(gripper_node.right_pub)
-        elif key.char == 'f':
-            open_gripper(gripper_node.right_pub)
-    except AttributeError:
-        pass
+def close_gripper(publisher):
+    msg = create_msg(POS_MIN)
+    print(msg, publisher)
+    publisher.publish(msg)
 
-def init_node():
-    rospy.init_node('GripperNode')
-    gripper_node = GripperNode()
-    rospy.loginfo("GRIPPER NODE TURNED ON")
-    rospy.loginfo("########################################################################################")
-    rospy.loginfo("########################################################################################")
-    rospy.loginfo("########################################################################################")
-    rospy.loginfo("Welcome to the gripper node! This node will give you control of the robot grippers")
-    rospy.loginfo("You can use the keyboard input to give common commands to the grippers")
-    rospy.loginfo("1. Open Left Gripper")
-    rospy.loginfo("2. Close Left Gripper")
-    rospy.loginfo("3. Open Right Gripper")
-    rospy.loginfo("4. Close Right Gripper")
-    rospy.loginfo("########################################################################################")
-    with keyboard.Listener(
-        on_press=lambda key: on_key_press(key, gripper_node)
-    ) as listener:
-        listener.join()
+def open_gripper(publisher):
+    msg = create_msg(POS_MAX)
+    print(msg, publisher)
+    publisher.publish(msg)
 
-def spin_ros():
-    try:
-        init_node()
-    except rospy.ROSInterruptException:
-        pass
 
-if __name__ == "__main__" :
-    spin_ros()
---
+++ src/component_apis/grippers/key_gripper.py
@@ -0,0 +1,51 @@
+import rospy
+from pynput import keyboard
+from grippers import open_gripper, close_gripper, GripperNode
+
+""" 
+    @Zix
+    This node will listen for key presses which are matched with desired commands of the grippers.
+    Uses the ASDF keys for closing and opening of left and right grippers by publishing the correct message to the 
+    Gripper action server in real time. 
+"""
+
+def on_key_press(key, gripper_node):
+    try:
+        if key.char == 'a':
+            close_gripper(gripper_node.left_pub)
+        elif key.char == 's':
+            open_gripper(gripper_node.left_pub)
+        elif key.char == 'd':
+            close_gripper(gripper_node.right_pub)
+        elif key.char == 'f':
+            open_gripper(gripper_node.right_pub)
+    except AttributeError:
+        pass
+
+def print_welcome():
+    rospy.loginfo("GRIPPER NODE TURNED ON")
+    rospy.loginfo("########################################################################################")
+    rospy.loginfo("########################################################################################")
+    rospy.loginfo("########################################################################################")
+    rospy.loginfo("Welcome to the gripper node! This node will give you control of the robot grippers")
+    rospy.loginfo("You can use the keyboard input to give common commands to the grippers")
+    rospy.loginfo("1. Open Left Gripper")
+    rospy.loginfo("2. Close Left Gripper")
+    rospy.loginfo("3. Open Right Gripper")
+    rospy.loginfo("4. Close Right Gripper")
+    rospy.loginfo("########################################################################################")
+
+def init_node():
+    rospy.init_node('GripperNode')
+    gripper_node = GripperNode()
+    print_welcome()
+    with keyboard.Listener(
+        on_press=lambda key: on_key_press(key, gripper_node)
+    ) as listener:
+        listener.join()
+
+if __name__ == "__main__" :
+    try:
+        init_node()
+    except rospy.ROSInterruptException:
+        pass
