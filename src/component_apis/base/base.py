4b4821c0fc4a79b2d5c8cf3a82a0e84fb115ed64
Dice Labs <dicelabs99@gmail.com>
2024-01-19 16:31:45 -0800

    Add Base Test

--- src/component_apis/base/base.py
+++ src/component_apis/base/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rospy
+from move_base_msgs.msg import MoveBaseGoal
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Quaternion
@@ -12,7 +13,7 @@ DEFAULT_SEQ_NUM = 0
 
 class BaseNode():
     def __init__(self):
-        self.base_pub = rospy.Publisher(HUSKY_BASE_TOPIC, PoseStamped, queue_size=10)
+        self.base_pub = rospy.Publisher(HUSKY_BASE_TOPIC, MoveBaseGoal, queue_size=10)
 
 class Position():
     def __init__(self, x, y, z):
@@ -28,24 +29,24 @@ class Orientation():
         self.w = w
 
 def create_base_msg(position, orientation):
-    msg = PoseStamped()
+    msg = MoveBaseGoal()
     fill_header(msg)
     fill_position(msg, position)
     fill_orientation(msg, orientation)
     return msg
 
 def fill_header(msg):
-    msg.header = Header()
-    msg.header.seq = DEFAULT_SEQ_NUM
-    msg.header.stamp = rospy.Time(0)
-    msg.header.frame_id = FRAME_ID
+    msg.target_pose.header = Header()
+    msg.target_pose.header.seq = DEFAULT_SEQ_NUM
+    msg.target_pose.header.stamp = rospy.Time(0)
+    msg.target_pose.header.frame_id = FRAME_ID
 
 def fill_position(msg, position):
     point = Point()
     point.x = position.x
     point.y = position.y
     point.z = position.z
-    msg.position = point
+    msg.target_pose.pose.position = point
 
 def fill_orientation(msg, orientation):
     quaternion = Quaternion()
@@ -53,7 +54,7 @@ def fill_orientation(msg, orientation):
     quaternion.y = orientation.y
     quaternion.z = orientation.z
     quaternion.w = orientation.w
-    msg.orientation = quaternion
+    msg.target_pose.pose.orientation = quaternion
 
 def publish_base_message(publisher, position, orientation):
     msg = create_base_msg(position, orientation)
@@ -70,5 +71,3 @@ def FORWARD(publisher):
     orientation = Orientation(0, 0, 0, 0)
     publish_base_message(publisher, position, orientation)
 
-"""  scp -r dicelabs administrator@146.244.98.51:/home/administrator/catkin_ws/temp
-"""
---
+++ src/component_apis/base/test_base.py
@@ -0,0 +1,24 @@
+# Import necessary modules
+from base import create_base_msg
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from move_base_msgs.msg import MoveBaseGoal
+import rospy
+from std_msgs.msg import Header
+
+def test_create_base_msg():
+    header = Header()
+    header.seq = 0
+    header.stamp = rospy.Time(0)
+    header.frame_id = 'base_link'
+    position = Point(0, 0, 0)
+    orientation = Quaternion(0.1, 0, 0, 0)
+
+    msg = create_base_msg(position, orientation)
+
+    expected_msg = MoveBaseGoal()
+    expected_msg.target_pose.header = header
+    expected_msg.target_pose.pose.position = position
+    expected_msg.target_pose.pose.orientation = orientation
+
+    assert expected_msg == msg
