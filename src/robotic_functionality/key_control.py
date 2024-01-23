0920ff716d422be20e46b849df779836950babb2
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-01-23 12:03:14 -0800

    Remove Unnecessary import from key_control

--- src/robotic_functionality/key_control.py
+++ src/robotic_functionality/key_control.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 import subprocess
-import key_arms, key_base, key_grippers
 
-PROGRAM_TO_INVOKE = "python3"
+INVOKE_PY_INTERPRETER = "python3"
 ARMS_SCRIPT = "key_arms.py"
 BASE_SCRIPT = "key_base.py"
 GRIPPERS_SCRIPT = "key_grippers"
@@ -12,7 +11,7 @@ scripts = [ARMS_SCRIPT, BASE_SCRIPT, GRIPPERS_SCRIPT]
 
 processes = []
 for script in scripts:
-    process = subprocess.Popen([PROGRAM_TO_INVOKE, script])
+    process = subprocess.Popen([INVOKE_PY_INTERPRETER, script])
     processes.append(process)
 
 for process in processes:
