2e00b2f87035eaf2b665e08bc776a22d4a8fe509
Dice Labs <dicelabs99@gmail.com>
2024-02-05 14:11:07 -0800

    Organize Key Control

--- src/robotic_functionality/key_control.py
+++ src/robotic_functionality/key_control.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 
-import os
 import subprocess
-import signal
 from pynput import keyboard
 
 INVOKE_PY_INTERPRETER = "python3"
@@ -10,21 +8,30 @@ L_ARM_SCRIPT = "key_arms.py -d l"
 R_ARM_SCRIPT = "key_arms.py -d r"
 BASE_SCRIPT = "key_base.py"
 GRIPPERS_SCRIPT = "key_grippers.py"
+EXIT_KEY = 'v'
 
-scripts = [L_ARM_SCRIPT, R_ARM_SCRIPT, BASE_SCRIPT, GRIPPERS_SCRIPT]
+def key_valid(key):
+     return hasattr(key, 'char') and key.char is not None
 
-processes = []
-for script in scripts:
-    process = subprocess.Popen([script])
-    processes.append(process)
-
-def on_key_press(key):
-    if hasattr(key, 'char') and key.char is not None:
-        if key.char == 'v':
+def on_key_press(key, processes):
+    if key_valid(key):
+        if key.char == EXIT_KEY:
             for pid in processes:
-                os.kill(int(pid), signal.SIGTERM)
+                pid.terminate()
+            return False # Exit
 
-with keyboard.Listener(
-        on_press=lambda key: on_key_press(key)
+def fork_processes(scripts: []):
+    processes=[]
+    for script in scripts:
+        cmd = [INVOKE_PY_INTERPRETER] + script.split()
+        process = subprocess.Popen(cmd)
+        processes.append(process)
+    return processes
+
+if __name__ == "__main__":
+    scripts = [L_ARM_SCRIPT, R_ARM_SCRIPT, BASE_SCRIPT, GRIPPERS_SCRIPT]
+    processes = fork_processes(scripts)
+    with keyboard.Listener(
+        on_press=lambda key: on_key_press(key, processes)
     ) as listener:
         listener.join()
