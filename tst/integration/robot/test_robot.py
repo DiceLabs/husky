8f6bb828c02fc4898125f5e6b15029fe3f5d5fb2
Conner Sommerfield <>
2024-03-15 23:53:40 -0700

    Add Protocol Test

---
+++ tst/integration/robot/test_robot.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from multiprocessing import Process, Queue
+from robot import Robot, start_generic_component
+from factory import ComponentId
+
+class TestRobot(unittest.TestCase):
+    @patch('robot.ComponentFactory')
+    def test_init_components(self, mock_component_factory):
+        mock_component_factory.return_value = {0 : MagicMock(), 1 : MagicMock(), 2 : MagicMock()}
+
+        robot = Robot()
+
+        self.assertEqual(len(robot.processes), 3)
+        self.assertEqual(len(robot.queues), 3)
+        for process in robot.processes:
+            self.assertTrue(isinstance(process, Process))
+
+if __name__ == '__main__':
+    unittest.main()
--- tst/run_tests.sh
+++ tst/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BIN_DIR="/home/dicelabs/ROBOT_LIB"
+BIN_DIR="$HOME/ROBOT_LIB"
 
 find "$BIN_DIR" -type f -name 'test*.py' | while read filename; do
     echo "Executing $filename"
