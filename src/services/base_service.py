756490c564a16aeb21b1190dace9c54677176021
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-03-27 20:34:58 -0700

    Move Service Loops to Timers

--- src/services/base_service.py
+++ src/services/base_service.py
@@ -2,17 +2,19 @@
 
 from multiprocessing import Process
 from services import ServiceNames, ServicePorts
-from server import start_server
+from generic_ros_server import start_ros_server
 from base import BaseNode
 from req_resp import GenericRequest
+import rospy
 
 def generic_callback(request: GenericRequest, base: BaseNode):
     function_call = getattr(base, request.function)
     function_call(**request.args)
 
 def start_base_server(name, port):
+    rospy.init_node(name)
     base = BaseNode()
-    start_server(name=name, port=port, callback=lambda request: generic_callback(request, base))
+    start_ros_server(name=name, port=port, callback=lambda request: generic_callback(request, base))
 
 def start_base(service_name, service_port):
     process = Process(target=start_base_server, args=(service_name, service_port))
--- src/services/gripper_service.py
+++ src/services/gripper_service.py
@@ -2,17 +2,19 @@
 
 from multiprocessing import Process
 from services import ServiceNames, ServicePorts
-from server import start_server
+from generic_ros_server import start_ros_server
 from grippers import GripperNode
 from req_resp import GenericRequest
+import rospy
 
 def generic_callback(request: GenericRequest, base: GripperNode):
     function_call = getattr(base, request.function)
     function_call(**request.args)
 
 def start_gripper_server(name, port):
+    rospy.init_node(name)
     gripper = GripperNode()
-    start_server(name=name, port=port, callback=lambda request: generic_callback(request, gripper))
+    start_ros_server(name=name, port=port, callback=lambda request: generic_callback(request, gripper))
 
 def start_gripper(service_name, service_port):
     process = Process(target=start_gripper_server, args=(service_name, service_port))
