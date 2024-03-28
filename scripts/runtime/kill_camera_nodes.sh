83a49541fc6e0d214d2d9d7fd54ea5b9be160dbf
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-03-27 21:06:47 -0700

    Kill ROS Nodes Script

---
+++ scripts/runtime/kill_camera_nodes.sh
@@ -0,0 +1,4 @@
+rosnode kill /realsense/realsense2_camera
+rosnode kill /realsense/realsense2_camera_manager
+rosnode kill /realsense_secondary/realsense2_camera
+rosnode kill /realsense_secondary/realsense2_camera_manager
