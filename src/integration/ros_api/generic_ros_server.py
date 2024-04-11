180054932c4d68fdedb1c7e66fee53124a6c6524
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-04-10 19:36:15 -0700

    Add Error Handling to ROS Server

--- src/integration/ros_api/generic_ros_server.py
+++ src/integration/ros_api/generic_ros_server.py
@@ -28,9 +28,14 @@ def start_ros_server(name=Defaults.DEFAULT_NAME,
         sock.bind((host, port))
         sock.listen()
         Logging.log_server_active_message(name, host, port)
-        loop(name, sock, callback)
-        rospy.Timer(rospy.Duration(PERIOD), lambda event: loop(name, sock, callback))
-        rospy.spin()
+        try:
+            loop(name, sock, callback)
+            rospy.Timer(rospy.Duration(PERIOD), lambda event: loop(name, sock, callback))
+            rospy.spin()
+        except KeyboardInterrupt:
+            print("Shutting down ROS server...")
+        finally:
+            sock.close()
 
 if __name__ == "__main__":
     start_ros_server()
